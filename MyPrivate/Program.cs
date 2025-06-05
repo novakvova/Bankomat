449cc94d09c5a01f311f0d0bbace1b382dcdd19d
valentinooo <valeartem@gmail.com>
2025-06-05 17:51:06 +0300

    змінив фіксацію  не по TcpClient а по публічній IP-адресі

--- MyPrivate/Program.cs
+++ MyPrivate/Program.cs
@@ -7,7 +7,8 @@ using System.IO;
 using System.Collections.Concurrent;
 TcpListener tcpListener = new TcpListener(IPAddress.Any, 5000); // Listening on port 5000 | пам'ять виділяється динамічно
 const int MinIntervalMs = 5000; // Minimum interval between connections in milliseconds
-ConcurrentDictionary<TcpClient, DateTime> clientLastAccess = new();
+ConcurrentDictionary<IPEndPoint, DateTime> clientLastAccess = new(); //фіксація по публічним IP - адресам клієнтів
+IPEndPoint iP;
 DateTime now;
 tcpListener.Start();
 Console.WriteLine($"Server started on port 5000. Waiting for connections...");
@@ -15,12 +16,13 @@ while (true)
 {
     try
     {
-        TcpClient client = await tcpListener.AcceptTcpClientAsync();
+        TcpClient client = await tcpListener.AcceptTcpClientAsync(); //звільняємо потік при очікуванні клієнта
         /// Check if the client is already connected or if the last access time is within the minimum interval
-        if (clientLastAccess.ContainsKey(client))
+        iP = client.Client.RemoteEndPoint as IPEndPoint; //зберігання IP-адреси клієнта
+        if (clientLastAccess.ContainsKey(iP))
         {
             now = DateTime.Now;
-            if (clientLastAccess.TryGetValue(client, out DateTime lastAccess))
+            if (clientLastAccess.TryGetValue(iP, out DateTime lastAccess))
             {
                 if ((now - lastAccess).TotalMilliseconds < MinIntervalMs)
                 {
