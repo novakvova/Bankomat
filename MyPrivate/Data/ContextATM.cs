1d0f9f52125da283531ad47a5f42b7eabf9c3fca
novakvova <novakvova@gmail.com>
2025-06-12 15:58:31 +0300

    Update Server

---
+++ MyPrivate/Data/ContextATM.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPrivate.Data.Entitys;
+
+namespace MyPrivate.Data
+{
+    public class ContextATM : DbContext
+    {
+        public DbSet<BalanceEntity> Balances { get; set; }
+        public DbSet<UserEntity> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=ep-quiet-sunset-a8fs2ggz-pooler.eastus2.azure.neon.tech;Database=neondb;Username=neondb_owner;Password=npg_8kphXGT2DNJo");
+        }
+    }
+}
