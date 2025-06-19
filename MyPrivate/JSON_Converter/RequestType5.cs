c3e8379d4fb1e49d519e3dc6379930f714e5125a
valentinooo <valeartem@gmail.com>
2025-06-19 17:16:23 +0300

    додав резервну копію свого коду для реєстрації(на всякий випадок)

--- MyPrivate/JSON_Converter/RequestType5.cs
+++ MyPrivate/JSON_Converter/RequestType5.cs
@@ -7,8 +7,14 @@ using System.Threading.Tasks;
 
 namespace MyClient.JSON_Converter
 {
-	public class RequestType5 : RequestBase
+	public class RequestType5 : RequestBase //запит на реєстрацію
 	{
 		public override Int32 Type { get; } = 5;
-	}
+        public string FirstName { get; set; } = string.Empty; // Default value for first name
+        public string LastName { get; set; } = string.Empty; // Default value for last name
+
+        public string FatherName { get; set; } = string.Empty; // Default value for father's name
+
+        public Int64 PinCode { get; set; } = 0; // Default value for PIN code
+    }
 }
